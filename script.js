0b9ef798d0656640e3e4ff72d5117abc541f06a8
4Sehat5Sekarat <135516534+4Sehat5Sekarat@users.noreply.github.com>
2023-07-01 00:11:58 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -8,33 +8,40 @@ copyButton.addEventListener('click', copyResult);
 themeButton.addEventListener('click', changeTheme);
 
 function calculateValues() {
-	const firstSkillId = parseInt(document.getElementById('5w1h-first-skill-id').value, 10);
-	const secondSkillId = parseInt(document.getElementById('5w1h-second-skill-id').value, 10);
-	const firstSkillLvl = parseInt(document.getElementById('5w1h-first-skill-lvl').value, 10);
-	const secondSkillLvl = parseInt(document.getElementById('5w1h-second-skill-lvl').value, 10);
-	const talismanRarity = parseInt(document.getElementById('5w1h-talisman-rarity').value, 10);
-	const numberOfSlots = parseInt(document.getElementById('5w1h-number-of-slots').value, 10);
+    const firstSkillId = parseInt(document.getElementById('5w1h-first-skill-id').value, 10);
+    const secondSkillId = parseInt(document.getElementById('5w1h-second-skill-id').value, 10);
+    const firstSkillLvl = parseInt(document.getElementById('5w1h-first-skill-lvl').value, 10);
+    const secondSkillLvl = parseInt(document.getElementById('5w1h-second-skill-lvl').value, 10);
+    const talismanRarity = parseInt(document.getElementById('5w1h-talisman-rarity').value, 10);
+    const numberOfSlots = parseInt(document.getElementById('5w1h-number-of-slots').value, 10);
+    const boxSlots = parseInt(document.getElementById('5w1h-box-slot').value, 10);
 
-	const xxxx = firstSkillId + secondSkillId * 128 + 16384 * numberOfSlots;
-	const yyyy = (firstSkillLvl + 30) * 16 + 1024 * (secondSkillLvl + 30) + talismanRarity - 1;
+    const xxxx = firstSkillId + secondSkillId * 128 + 16384 * numberOfSlots;
+    const yyyy = (firstSkillLvl + 30) * 16 + 1024 * (secondSkillLvl + 30) + talismanRarity - 1;
+    const zzaa = 65032 + (12 * boxSlots);
+    const zzbb = 65036 + (12 * boxSlots);
 
-	const xxxxHex = xxxx.toString(16).toUpperCase();
-	const yyyyHex = yyyy.toString(16).toUpperCase();
+    const xxxxHex = xxxx.toString(16).toUpperCase();
+    const yyyyHex = yyyy.toString(16).toUpperCase();
+    const zzaaHex = zzaa.toString(16).toUpperCase();
+    const zzbbHex = zzbb.toString(16).toUpperCase();
 
-	output.textContent = `_C0 Custom talisman\n_L 0x2174FE14 0x${yyyyHex}6501\n_L 0x2174FE18 0x0000${xxxxHex}`;
+    const Nama = document.getElementById("5w1h-talisman-rarity" + talismanRarity).textContent;
+
+    output.textContent = `_C0 Custom ${Nama} [${numberOfSlots} Slot]\n_L 0x2174${zzaaHex} 0x${yyyyHex}6501\n_L 0x2174${zzbbHex} 0x0000${xxxxHex}`;
 }
 
 function copyResult() {
-	const range = document.createRange();
-	range.selectNode(output);
-	window.getSelection().removeAllRanges();
-	window.getSelection().addRange(range);
-	document.execCommand('copy');
-	window.getSelection().removeAllRanges();
+    const range = document.createRange();
+    range.selectNode(output);
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(range);
+    document.execCommand('copy');
+    window.getSelection().removeAllRanges();
 }
 
 function changeTheme() {
-	var body = document.body;
-	body.classList.toggle("light");
-	body.classList.toggle("dark");
+    var body = document.body;
+    body.classList.toggle("light");
+    body.classList.toggle("dark");
 }
