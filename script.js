5ddee1f94ea1b129669566340545fd2d56963460
4Sehat5Sekarat <135516534+4Sehat5Sekarat@users.noreply.github.com>
2023-07-06 21:18:11 +0700

    script.js

--- script.js
+++ script.js
@@ -8,40 +8,62 @@ copyButton.addEventListener('click', copyResult);
 themeButton.addEventListener('click', changeTheme);
 
 function calculateValues() {
-    const firstSkillId = parseInt(document.getElementById('5w1h-first-skill-id').value, 10);
-    const secondSkillId = parseInt(document.getElementById('5w1h-second-skill-id').value, 10);
-    const firstSkillLvl = parseInt(document.getElementById('5w1h-first-skill-lvl').value, 10);
-    const secondSkillLvl = parseInt(document.getElementById('5w1h-second-skill-lvl').value, 10);
-    const talismanRarity = parseInt(document.getElementById('5w1h-talisman-rarity').value, 10);
-    const numberOfSlots = parseInt(document.getElementById('5w1h-number-of-slots').value, 10);
-    const boxSlots = parseInt(document.getElementById('5w1h-box-slot').value, 10);
+  const firstSkillId = parseInt(document.getElementById('5w1h-first-skill-id').value, 10);
+  const secondSkillId = parseInt(document.getElementById('5w1h-second-skill-id').value, 10);
+  let firstSkillLvl = parseInt(document.getElementById('5w1h-first-skill-lvl').value, 10);
+  let secondSkillLvl = parseInt(document.getElementById('5w1h-second-skill-lvl').value, 10);
+  const talismanRarity = parseInt(document.getElementById('5w1h-talisman-rarity').value, 10);
+  const numberOfSlots = parseInt(document.getElementById('5w1h-number-of-slots').value, 10);
+  const boxSlots = parseInt(document.getElementById('5w1h-box-slot').value, 10);
 
-    const xxxx = firstSkillId + secondSkillId * 128 + 16384 * numberOfSlots;
-    const yyyy = (firstSkillLvl + 30) * 16 + 1024 * (secondSkillLvl + 30) + talismanRarity - 1;
-    const zzaa = 65032 + (12 * boxSlots);
-    const zzbb = 65036 + (12 * boxSlots);
+  if (isNaN(firstSkillLvl) || firstSkillLvl === "") {
+    firstSkillLvl = 0;
+  }
+  if (isNaN(secondSkillLvl) || secondSkillLvl === "") {
+    secondSkillLvl = 0;
+  }
 
-    const xxxxHex = xxxx.toString(16).toUpperCase();
-    const yyyyHex = yyyy.toString(16).toUpperCase();
-    const zzaaHex = zzaa.toString(16).toUpperCase();
-    const zzbbHex = zzbb.toString(16).toUpperCase();
+  const xxxx = firstSkillId + secondSkillId * 128 + 16384 * numberOfSlots;
+  const yyyy = (firstSkillLvl + 30) * 16 + 1024 * (secondSkillLvl + 30) + talismanRarity - 1;
+  const zzaa = 65032 + (12 * boxSlots);
+  const zzbb = 65036 + (12 * boxSlots);
 
-    const Nama = document.getElementById("5w1h-talisman-rarity" + talismanRarity).textContent;
+  const xxxxHex = xxxx.toString(16).toUpperCase().padStart(4, "0");
+  const yyyyHex = yyyy.toString(16).toUpperCase().padStart(4, "0");
+  const zzaaHex = zzaa.toString(16).toUpperCase().padStart(4, "0");
+  const zzbbHex = zzbb.toString(16).toUpperCase().padStart(4, "0");
 
-    output.textContent = `_C0 Custom ${Nama} [${numberOfSlots} Slot]\n_L 0x2174${zzaaHex} 0x${yyyyHex}6501\n_L 0x2174${zzbbHex} 0x0000${xxxxHex}`;
+  const Nama = document.getElementById("5w1h-talisman-rarity" + talismanRarity).textContent;
+
+  if (firstSkillLvl || secondSkillLvl) {
+    if (yyyyHex.length > 4) {
+      output.textContent = `Too many skill levels`;
+    } else {
+      output.textContent = `_C0 Custom ${Nama} [${numberOfSlots} Slot]\n_L 0x2174${zzaaHex} 0x${yyyyHex}6501\n_L 0x2174${zzbbHex} 0x0000${xxxxHex}`;
+    }
+  } else {
+    output.textContent = `Input skill levels, please`;
+  }
 }
 
 function copyResult() {
-    const range = document.createRange();
-    range.selectNode(output);
-    window.getSelection().removeAllRanges();
-    window.getSelection().addRange(range);
-    document.execCommand('copy');
-    window.getSelection().removeAllRanges();
+  const range = document.createRange();
+  range.selectNode(output);
+  window.getSelection().removeAllRanges();
+  window.getSelection().addRange(range);
+  document.execCommand('copy');
+  window.getSelection().removeAllRanges();
+
+  Swal.fire({
+    position: 'top-right',
+    icon: 'success',
+    title: 'Copied to clipboard',
+    showConfirmButton: false,
+    timer: 2500
+  });
 }
 
 function changeTheme() {
-    var body = document.body;
-    body.classList.toggle("light");
-    body.classList.toggle("dark");
+  document.body.classList.toggle("light");
+  document.body.classList.toggle("dark");
 }
